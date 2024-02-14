0fe7d1dabd1a031a477dd62f8653d6f26923e980
dahalkumar <kumardahal536@gmail.com>
2024-02-14 13:38:36 -0500

    for class project

---
+++ tests/unit/test_unit.py
@@ -0,0 +1,56 @@
+import pytest
+from pathlib import Path
+from box import ConfigBox
+from ensure.main import EnsureError
+import os
+from box.exceptions import BoxValueError
+import yaml
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            # logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+class Test_read_yaml:
+    yaml_files = [
+        "tests/data/empty.yaml",
+        "tests/data/demo.yaml"
+    ]
+
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):
+            read_yaml(Path(self.yaml_files[0]))
+
+    def test_read_yaml_return_type(self):
+        respone = read_yaml(Path(self.yaml_files[-1]))
+        assert isinstance(respone, ConfigBox)
+
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)
+    def test_read_yaml_bad_type(self, path_to_yaml):
+        with pytest.raises(EnsureError):
+            read_yaml(path_to_yaml)
